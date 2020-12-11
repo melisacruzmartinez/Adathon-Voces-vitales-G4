65a2fd98b8bde9f419248b4e5da9393378cf4a5c
melisacruzmartinez <melisa.cruzmartinez@gmail.com>
2020-12-11 14:48:42 -0300

    esqueleto

---
+++ vocesvitales/src/main/java/ar/com/ada/api/vocesvitales/VocesvitalesApplication.java
@@ -0,0 +1,13 @@
+package ar.com.ada.api.vocesvitales;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class VocesvitalesApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(VocesvitalesApplication.class, args);
+	}
+
+}
---
+++ vocesvitales/src/test/java/ar/com/ada/api/vocesvitales/VocesvitalesApplicationTests.java
@@ -0,0 +1,13 @@
+package ar.com.ada.api.vocesvitales;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class VocesvitalesApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
